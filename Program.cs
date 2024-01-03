bd1b2f84700be729e1c404517e5a79cd11ae7391
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-01-03 05:38:25 +0100

    Updating AcademyDbContext class, startAcademyApp class and main program.
    - Updating AcademyDbContext class: Data model classes to define.
    - startAcademyApp class: Main menu created.
    - program class: to reduce pressure from the main program class, startAcademyApp class has been defined.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace AcademyManagementApp;
+﻿using AcademyManagementApp.Functions;
+using AcademyManagementApp.Data;
+
+namespace AcademyManagementApp;
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello, World!");
+
+        startAcademyApp.startApp();
+
+        
     }
 }
 
